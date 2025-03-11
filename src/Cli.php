dc6da95233703f12ea93f56d6d30959f8adf0105
MarinaRaskevich <marina.raskevich19@gmail.com>
2025-03-11 18:53:55 +0100

    feat: add solution for fourth step

--- src/Cli.php
+++ src/Cli.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace BrainGames\Cli;
+
 use function cli\line;
 use function cli\prompt;
 
@@ -8,4 +10,3 @@ function greeting()
     $name = prompt('May i have yout name?');
     line("Hello, %s!", $name);
 }
-
