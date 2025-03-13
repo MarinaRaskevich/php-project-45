1eb7fe4db48e131ffbce6f2e0e7b6fc73751c505
MarinaRaskevich <marina.raskevich19@gmail.com>
2025-03-13 17:11:34 +0100

    feat: add solution for 4 step (even game)

---
+++ src/BuildGame.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace BrainGames\BuildGame;
+
+use function cli\line;
+use function cli\prompt;
+
+const ROUNDS_NUMBER = 3;
+
+function buildGameProcess(string $gameRules, callable $generateRoundData): void
+{
+    echo "Welcome to the Brain Games!\n";
+    $name = prompt('May i have yout name?');
+    line("Hello, %s!", $name);
+    line($gameRules);
+
+    for ($i = 0; $i < ROUNDS_NUMBER; $i += 1) {
+        [$question, $correctAnswer] = $generateRoundData();
+
+        line("Question: {$question}");
+        $userAnswer = prompt('Your answer');
+
+        if ($userAnswer === $correctAnswer) {
+            line('Correct!');
+        } else {
+            line("'{$userAnswer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'");
+            line("Let's try again, {$name}!");
+            return;
+        }
+    }
+
+    line("Congratulations, {$name}");
+}
---
+++ src/Games/EvenGame.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace BrainGames\Games\EvenGame;
+
+use function BrainGames\BuildGame\buildGameProcess;
+
+const GAME_RULES = 'Answer "yes" if the number is even, otherwise answer "no".';
+const MAX_VALUE = 50;
+const MIN_VALUE = 1;
+
+function generateEvenGameRound(): array
+{
+    $randomNumber = rand(MIN_VALUE, MAX_VALUE);
+    $correctAnswer = ($randomNumber % 2 === 0) ? 'yes' : 'no';
+
+    return [$randomNumber, $correctAnswer];
+}
+
+function play(): void
+{
+    buildGameProcess(GAME_RULES, function () {
+        return generateEvenGameRound();
+    });
+}
